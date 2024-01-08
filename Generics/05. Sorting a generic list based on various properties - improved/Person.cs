7e4048bfdf5bda9de812bcc1dacdfe1489b5a53b
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-08 12:52:46 +0100

    Generics: feat: Sort a generic list based on various type - improved

---
+++ Generics/05. Sorting a generic list based on various properties - improved/Person.cs
@@ -0,0 +1,28 @@
+﻿namespace _05._Sorting_a_generic_list_based_on_various_properties___improved
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+
+        public static int CompareByName(Person x, Person y)
+        {
+            if (x == null && y == null) return 0;
+            if (x == null) return -1;
+            if (y == null) return 1;
+
+            return x.Name.CompareTo(y.Name);
+        }
+
+        public static int CompareByCity(Person x, Person y)
+        {
+            if (x == null && y == null) return 0;
+            if (x == null) return -1;
+            if (y == null) return 1;
+
+            return x.City.CompareTo(y.City);
+        }
+    }
+}
---
+++ Generics/05. Sorting a generic list based on various properties - improved/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _05._Sorting_a_generic_list_based_on_various_properties___improved
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> list = new()
+            {
+                new Person { Name = "Meier", City = "Berlin" },
+                null,
+                new Person { Name = "Schulz", City = "Stuttgart" },
+                new Person { Name = "Gerhards", City = "Hamburg" },
+                null,
+                new Person { Name = "Müller", City = "Bremen" },
+            };
+
+
+            list.Sort(Person.CompareByCity);
+
+            Console.WriteLine("Sort by City:");
+            foreach (Person tempPerson in list)
+            {
+                if (tempPerson != null)
+                {
+                    Console.Write($"Name = {tempPerson.Name,-12}");
+                    Console.WriteLine($"City = {tempPerson.City}");
+                }
+            }
+
+
+            list.Sort(Person.CompareByName);
+
+            Console.WriteLine("Sort by Name:");
+            foreach (Person tempPerson in list)
+            {
+                if (tempPerson != null)
+                {
+                    Console.Write($"Name = {tempPerson.Name,-12}");
+                    Console.WriteLine($"City = {tempPerson.City}");
+                }
+            }
+        }
+    }
+}
