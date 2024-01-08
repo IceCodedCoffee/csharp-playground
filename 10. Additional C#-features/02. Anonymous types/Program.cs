80988ba5b5160a2b10b3576af2845411d1e87851
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-08 17:01:04 +0100

    Additional C#-features: Add anonymous types

---
+++ 10. Additional C#-features/02. Anonymous types/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02._Anonymous_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new { Name = "Peter", Ort = "Hamburg" };
+
+
+            var obj1 = new { Name = "Peter", Ort = "Hamburg" };
+            var obj2 = new { Name = "Uwe", Ort = "München" };
+            var obj3 = new { Ort = "Berlin", Name = "Hans" };
+
+            Console.WriteLine(obj.GetType());
+            Console.WriteLine(obj1.GetType());
+            Console.WriteLine(obj2.GetType());
+            Console.WriteLine(obj3.GetType());
+        }
+    }
+}
