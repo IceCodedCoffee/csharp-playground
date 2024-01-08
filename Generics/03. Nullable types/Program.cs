4b6a4322729f991b3c06d89308f5bc1dde3f3829
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-08 09:25:49 +0100

    Generics: feat: Add nullable types

---
+++ Generics/03. Nullable types/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _03._Nullable_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x = null;
+
+            if (x.HasValue)
+            {
+                Console.WriteLine($"Value of x: {x.Value}");
+            }
+            else
+            {
+                Console.WriteLine("x is null!");
+            }
+
+            int? y = 4711;
+
+            if (y.HasValue)
+            {
+                Console.WriteLine($"Value of y: {y.Value}");
+            }
+            else
+            {
+                Console.WriteLine("y is null!");
+            }
+        }
+    }
+}
