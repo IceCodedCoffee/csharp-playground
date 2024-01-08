d498a7fc0183cd2af6f9fdb3a6c8a0427122cec9
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-08 09:20:25 +0100

    Generics: feat: Add generic class def

---
+++ Generics/01. Generic class def/Stack.cs
@@ -0,0 +1,43 @@
+﻿namespace Generics
+{
+    class Stack<T>
+    {
+        private readonly int size;
+        private T[] elements;
+        private int pointer = 0;
+
+        public Stack(int size)
+        {
+            this.size = size;
+            elements = new T[size];
+        }
+
+        public void Push(T element)
+        {
+            if (pointer >= this.size)
+            {
+                throw new StackOverflowException();
+            }
+
+            elements[pointer] = element;
+            pointer++;
+        }
+
+        public T Pop()
+        {
+            pointer--;
+            if (pointer >= 0)
+            {
+                return elements[pointer];
+            }
+
+            else
+            {
+                pointer = 0;
+                throw new InvalidOperationException("Stack is empty!");
+            }
+        }
+
+        public int Length => this.pointer;
+    }
+}
