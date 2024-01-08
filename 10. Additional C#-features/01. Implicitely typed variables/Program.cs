f15010192eda444ddaff22add78dbd5ce53a2ced
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-08 14:00:30 +0100

    Additional C#-features: feat: Implicitely type variables

---
+++ 10. Additional C#-features/01. Implicitely typed variables/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _01._Implicitely_typed_variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 5;
+
+            var city = "Aachen";
+
+            var arr = new[] { 0, 1, 2 };
+
+
+            //var x;
+
+            //var x = null;
+
+            //class Person
+            //{
+            //    var Age;
+            //}
+
+            //public var ChangeName(var Name) { }
+
+
+
+            for (var y = 0; y < 100; y++)
+            {
+                Console.WriteLine(y);
+            }
+
+            foreach (var element in arr) 
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
