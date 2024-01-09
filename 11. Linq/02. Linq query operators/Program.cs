628e7c2ff80f240dbe673f8a9a1f31dfb394a82e
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-09 17:07:41 +0100

    Linq: feat: Add Linq query operators - sample data

---
+++ 11. Linq/02. Linq query operators/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02._Linq_query_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
