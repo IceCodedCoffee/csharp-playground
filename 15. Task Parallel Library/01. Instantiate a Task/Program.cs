76a30bc7d3b7e4ee835c50a4e30d403918ae377c
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-14 18:08:48 +0100

    TPL: Task: Instantiate a task

---
+++ 15. Task Parallel Library/01. Instantiate a Task/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+
+namespace _01._Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task1 = new Task(new Action(DoSomething));
+
+            task1.Start();
+
+            Console.WriteLine();
+
+            // ----------------------------------------------------------------------------------------------
+
+            Task.Factory.StartNew(new Action(DoSomething));
+
+            Console.WriteLine();
+
+            // ----------------------------------------------------------------------------------------------
+
+            Task.Run(() =>
+            {
+                var id = Thread.CurrentThread.ManagedThreadId;
+
+                Console.WriteLine($"Task #{id}");
+            });
+
+            Console.WriteLine();
+
+            // ----------------------------------------------------------------------------------------------
+
+            Parallel.Invoke(Task1);
+            Console.ReadLine();
+        }
+
+        private static void DoSomething()
+        {
+            Console.WriteLine($"In method DoSomething");
+        }
+
+        static void Task1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(50);
+                Console.Write(" #1 ");
+            }
+        }
+    }
+}
