645e6b86b6ac2c27decdc18bfff30f788fde4e90
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-14 18:32:15 +0100

    TPL: Task: Add tasks with return value

---
+++ 15. Task Parallel Library/02. Tasks with return value/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _02._Tasks_with_return_value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<string> task = Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                return DateTime.Now.ToLongTimeString();
+            });
+
+            Console.WriteLine("Task still running ...");
+
+            Console.WriteLine($"Result: {task.Result}");
+
+            Console.WriteLine("Task finished ...");
+
+            Console.WriteLine();
+        }
+    }
+}
