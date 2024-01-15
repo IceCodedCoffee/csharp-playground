1f0d0e3bb85c7073fe1c0db0e8d724eda6d72a0e
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-15 13:28:18 +0100

    TPL: Async: Add async operations with return values

---
+++ 15. Task Parallel Library/15. Async - Async operations with return values/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _15._Async___Async_operations_with_return_values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+            Console.WriteLine("End of Main ...");
+            Console.ReadLine();
+        }
+
+        static async void Start()
+        {
+            long result = await CalculateAsync(12, 88);
+            Console.WriteLine($"Result: {result}");
+
+            //Task<long> task = CalculateAsync(12, 88);
+            //Console.WriteLine("Something else");
+            //await task;
+            //Console.WriteLine($"Result: {task.Result}");
+        }
+
+        static Task<long> CalculateAsync(int x, int y)
+        {
+            Task<long> task = Task.Run<long>(() =>
+            {
+                Thread.Sleep(2500);
+                return x + y;
+            });
+
+            return task;
+        }
+    }
+}
