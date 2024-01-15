6b2a72b706fcbf5cbcf1c35913deb79ee2f78024
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-15 11:24:04 +0100

    TPL: Parallel: Set the degree of parallelism

---
+++ 15. Task Parallel Library/10. Parallel - Cancelling a parallel for-loop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace _10._Parallel___Cancelling_a_parallel_for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = new double[100_000];
+            int currentIteration = 0;
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+
+            Parallel.For(0, arr.Length, (i, option) =>
+            {
+                currentIteration = i;
+                arr[i] = Math.Pow(i, 0.333) * Math.Sqrt(Math.Sin(i));
+
+                if (i > 1000)
+                    option.Stop();
+            });
+
+            watch.Stop();
+            Console.WriteLine($"Iteration: {currentIteration}, Parallel: {watch.ElapsedMilliseconds} ms");
+        }
+    }
+}
---
+++ 15. Task Parallel Library/11. Setting the degree of parallelism/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace _11._Setting_the_degree_of_parallelism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount };
+            double[] arr = new double[100_000_000];
+            int currentIteration = 0;
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+
+            Parallel.For(0, arr.Length, options, i=>
+            {
+                currentIteration = i;
+                arr[i] = Math.Pow(i, 0.333) * Math.Sqrt(Math.Sin(i));
+            });
+
+            watch.Stop();
+            Console.WriteLine($"ProcessorCount = {Environment.ProcessorCount}, Parallel: {watch.ElapsedMilliseconds} ms");
+        }
+    }
+}
