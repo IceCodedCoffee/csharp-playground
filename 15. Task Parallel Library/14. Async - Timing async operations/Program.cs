0ab55c4091cf22945d0a77c97c38bd1949d1f141
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-15 13:08:04 +0100

    TPL: Async: Time async operations

---
+++ 15. Task Parallel Library/14. Async - Timing async operations/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _14._Async___Timing_async_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+            Console.WriteLine("End of Main ...");
+            Console.ReadLine();
+        }
+
+        static async void Start()
+        {
+            Task task = DoSomethingAsync(1);
+            Console.WriteLine("after calling DoSomething(1) ...");
+            await task;
+            Console.WriteLine("Async operation finished");
+        }
+
+        static Task DoSomethingAsync(int id)
+        {
+            Task task = Task.Run(() =>
+            {
+                Console.WriteLine($"Operation {id} commences ...");
+                Thread.Sleep(2500);
+                Console.WriteLine($"Operation {id} is finished.");
+            });
+
+            return task;
+        }
+    }
+}
