2cf126d301fd2d9a04fd3685f09f9bed62707238
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-15 10:38:15 +0100

    TPL: Parallel: Set a different step size for a parallel loop

---
+++ 15. Task Parallel Library/09. Parallel - Setting a different step size for a parallel loop/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _09._Parallel___Setting_a_different_step_size_for_a_parallel_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 50, i =>
+            {
+                int newIndex = i * 2;
+                Console.Write($" {newIndex} ");
+            });
+        }
+    }
+}
