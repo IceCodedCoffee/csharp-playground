e4117b1fbb0e1e96b92592e532efc40cce083a08
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-15 12:57:34 +0100

    TPL: Async: Declare an async task & method

---
+++ 15. Task Parallel Library/13. Async - Declaring an async task & method/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _13._Async___Declaring_an_async_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+            Console.WriteLine("End of Main ...");
+            Console.ReadLine();
+        }
+
+        static async void Start()
+        {
+            await DoSomethingAsync(1);
+            Console.WriteLine("after calling DoSomething(1) ...");
+        }
+
+        static Task DoSomethingAsync(int id)
+        {
+            Task task = Task.Run(() =>
+            {
+                Console.WriteLine($"Operation {id} commences ...");
+                Thread.Sleep(2500);
+                Console.WriteLine($"Operation {id} is finished.");
+            });
+
+            return task;
+        }
+    }
+}
