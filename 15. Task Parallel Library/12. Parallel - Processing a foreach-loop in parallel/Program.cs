38d282dc21a60c9048f8655810588c314c60732c
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-15 11:27:02 +0100

    TPL: Parallel: Process a foreach-loop in parallel

---
+++ 15. Task Parallel Library/12. Parallel - Processing a foreach-loop in parallel/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _12._Parallel___Processing_a_foreach_loop_in_parallel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namen = { "Peter", "Uwe", "Udo", "Willi", "Pia", "Michael", "Conie" };
+
+            Parallel.ForEach(namen, name =>
+            {
+                Console.WriteLine(name);
+            });
+        }
+    }
+}
