7e48b32c777f562f35a36ebbc9101d6a8e2023a1
IceCodedCoffee <wolf.alexander.development@gmail.com>
2024-01-16 15:40:14 +0100

    Additional C#-features: Add conditional-, null-conditional- & null-coalescing-operators

---
+++ 10. Additional C#-features/04. Conditional-, null-conditional- & null-coalescing-operators/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _04._Nullable_types__conditional____null_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+
+            string result = (number > 5) ? "Greater than 5" : "Less than or equal to 5";
+
+            Console.WriteLine(result);
+            Console.WriteLine();
+
+            
+            int y, x = 5;
+            y = (x == 0) ? 1 : x;
+
+
+            string test = null;
+
+            int length = test != null ? test.Length : -1;
+
+            // ---------------------------------------------------------------------------------
+
+            Person person = null;
+            string personName = person?.Name;
+
+            Console.WriteLine(personName);
+            Console.WriteLine();
+
+            // ---------------------------------------------------------------------------------
+
+            string expression = null;
+            string defaultValue = "Expression 1 is null";
+
+            result = expression ?? defaultValue;
+
+            Console.WriteLine(result);
+            Console.WriteLine();
+
+            // ---------------------------------------------------------------------------------
+
+            string defaultValue2 = "Person instance is null";
+
+            result = person?.Name ?? defaultValue2;
+
+            Console.WriteLine(result);
+            Console.WriteLine();
+        }
+
+        class Person
+        {
+            public string Name { get; set; }
+        }
+    }
+}
